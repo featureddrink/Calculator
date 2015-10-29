2aff3cf91eb2c6cce9de1bb74e2525828c07249b
featureddrink <david@featureddrink.com>
2015-10-29 14:31:52 -0500

    Design Changes
    Better Layout

--- Calculator/src/UserInterface.java
+++ Calculator/src/UserInterface.java
@@ -38,6 +38,11 @@ public class UserInterface implements Runnable {
 		JButton clear = new JButton("C");
 		JButton reset = new JButton("CE");
 		JButton calculate = new JButton("Calculate");
+		JLabel davidsCalc = new JLabel("David's Calculator");
+		
+		JPanel display = new JPanel(new GridLayout(2, 1));
+		display.add(davidsCalc);
+		display.add(output);
 		
 		JPanel operators = new JPanel(new GridLayout(1, 4));
 		operators.add(add);
@@ -45,21 +50,31 @@ public class UserInterface implements Runnable {
 		operators.add(multiply);
 		operators.add(divide);
 		
-		JPanel numbers = new JPanel(new GridLayout(4, 3));
-		numbers.add(one);
-		numbers.add(two);
-		numbers.add(three);
-		numbers.add(four);
-		numbers.add(five);
-		numbers.add(six);
-		numbers.add(seven);
-		numbers.add(eight);
-		numbers.add(nine);
-		numbers.add(clear);
-		numbers.add(zero);
-		numbers.add(reset);
+		JPanel inputAndOperators = new JPanel(new GridLayout(2, 1));
+		inputAndOperators.add(input);
+		inputAndOperators.add(operators);
+		
+		JPanel numbers1 = new JPanel(new GridLayout(2, 3));
+		numbers1.add(one);
+		numbers1.add(two);
+		numbers1.add(three);
+		numbers1.add(four);
+		numbers1.add(five);
+		numbers1.add(six);
+		
+		JPanel numbers2 = new JPanel(new GridLayout(2, 3));
+		numbers2.add(seven);
+		numbers2.add(eight);
+		numbers2.add(nine);
+		numbers2.add(clear);
+		numbers2.add(zero);
+		numbers2.add(reset);
 		
 		output.setEnabled(false);
+		davidsCalc.setHorizontalAlignment(SwingConstants.CENTER);
+		davidsCalc.setFont(new Font("Arial", Font.BOLD, 18));
+		input.setHorizontalAlignment(SwingConstants.CENTER);
+		output.setHorizontalAlignment(SwingConstants.CENTER);
 		
 		ActionEventListener handler = new ActionEventListener(output, input, add, subtract, multiply, divide, one, two, three,
 				four, five, six, seven, eight, nine, zero, clear, reset);
@@ -83,10 +98,10 @@ public class UserInterface implements Runnable {
 		reset.addActionListener(handler);
 		calculate.addActionListener(handler);
 		
-		container.add(output);
-		container.add(input);
-		container.add(operators);
-		container.add(numbers);
+		container.add(display);
+		container.add(inputAndOperators);
+		container.add(numbers1);
+		container.add(numbers2);
 	}
 	
 	public JFrame getFrame() {
